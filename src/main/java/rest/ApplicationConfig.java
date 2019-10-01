e89b07956a787451eedb268d44d91c104402a91b
lars-m <lam@cphbusiness.dk>
2019-10-01 09:44:15 +0200

    Ready for OpenAPI Documentation

--- src/main/java/rest/ApplicationConfig.java
+++ src/main/java/rest/ApplicationConfig.java
@@ -1,5 +1,7 @@
 package rest;
 
+import io.swagger.v3.jaxrs2.integration.resources.AcceptHeaderOpenApiResource;
+import io.swagger.v3.jaxrs2.integration.resources.OpenApiResource;
 import java.util.Set;
 import javax.ws.rs.core.Application;
 
@@ -10,18 +12,22 @@ public class ApplicationConfig extends Application {
     public Set<Class<?>> getClasses() {
         Set<Class<?>> resources = new java.util.HashSet<>();
         addRestResourceClasses(resources);
+        
+        //These two Resource Classes are not auto discovered so we add them manually
+        resources.add(OpenApiResource.class);
+        resources.add(AcceptHeaderOpenApiResource.class);
+
         return resources;
     }
 
     /**
-     * Do not modify addRestResourceClasses() method.
-     * It is automatically populated with
-     * all resources defined in the project.
-     * If required, comment out calling this method in getClasses().
+     * Do not modify addRestResourceClasses() method. It is automatically
+     * populated with all resources defined in the project. If required, comment
+     * out calling this method in getClasses().
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
         resources.add(org.glassfish.jersey.server.wadl.internal.WadlResource.class);
         resources.add(rest.RenameMeResource.class);
     }
-    
+
 }
